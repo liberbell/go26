934fad0ade426e64a810556327b7a329e13ef50c
Liberbell <liberbell@gmail.com>
2023-12-28 07:43:35 +0900

    main4

--- lesson2/main.go
+++ lesson2/main.go
@@ -4,16 +4,16 @@ import "fmt"
 
 func main() {
 	aCostume := "Clip Clop"
-	fmt.Printf("Roger is wearing a costume as %s.\n", aCostume)
+	fmt.Printf("Roger is wearing a costume1 as %s.\n", aCostume)
 
 	changeTheCostume(aCostume)
-	fmt.Printf("Roger is wearing a costume as %s.\n", aCostume)
+	fmt.Printf("Roger is wearing a costume2 as %s.\n", aCostume)
 }
 
 func changeTheCostume(c string) {
-	fmt.Printf("Roger is wearing a costume as %s.\n", c)
+	fmt.Printf("Roger is wearing a costume3 as %s.\n", c)
 	newCostume := "Rickey Spanish"
 
 	c = newCostume
-	fmt.Printf("Roger is wearing a costume as %s.\n", c)
+	fmt.Printf("Roger is wearing a costume4 as %s.\n", c)
 }
