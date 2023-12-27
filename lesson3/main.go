26ced78eb4a1d41c2c20ee6c7c57cd040de2f83b
Liberbell <liberbell@gmail.com>
2023-12-28 07:48:15 +0900

    main1

---
+++ lesson3/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+var role1 = "Billionere"
+
+func main() {
+	var role2 = "Inventor"
+
+	fmt.Println("Bruce wayne is ", role1)
+	fmt.Println("Bruce wayne is ", role2)
+}
