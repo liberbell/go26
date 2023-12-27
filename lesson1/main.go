3d2eeee2dae1f458da71baf66cc5fd3f46d840b1
Liberbell <liberbell@gmail.com>
2023-12-27 10:25:29 +0900

    main6

--- lesson1/main.go
+++ lesson1/main.go
@@ -19,7 +19,7 @@ func main() {
 	fmt.Println(done, trueOrNot)
 }
 
-func outputThat(a int, b float64, c string) (string bool) {
+func outputThat(a int, b float64, c string) (string, bool) {
 	fmt.Printf("Integer: %d, FloatinPoint: %f, String: %s\n", a, b, c)
 	return "Ok,", true
 }
