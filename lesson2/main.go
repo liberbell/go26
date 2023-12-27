9d332b183c9fea781defd2bca077b1aef7ab94c3
Liberbell <liberbell@gmail.com>
2023-12-28 07:37:35 +0900

    main2

--- lesson2/main.go
+++ lesson2/main.go
@@ -9,3 +9,9 @@ func main() {
 	changeTheCostume(aCostume)
 	fmt.Printf("Roger is wearing a costume as %s.\n", aCostume)
 }
+
+func changeTheCostume(c string) {
+	fmt.Printf("Roger is wearing a costume as %s.\n", c)
+	newCostume := "Rickey Spanish"
+
+}
