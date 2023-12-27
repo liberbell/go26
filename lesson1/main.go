939842db373a43764918845381dbe1a4ff2ced47
Liberbell <liberbell@gmail.com>
2023-12-27 10:14:05 +0900

    main2

--- lesson1/main.go
+++ lesson1/main.go
@@ -1,36 +1,7 @@
 package main
 
-import (
-	"io"
-	"net/http"
-	"time"
-
-	scs "github.com/alexedwards/scs/v2"
-)
-
-var sessionManager *scs.SessionManager
+import "fmt"
 
 func main() {
-	// Initialize a new session manager and configure the session lifetime.
-	sessionManager = scs.New()
-	sessionManager.Lifetime = 24 * time.Hour
-
-	mux := http.NewServeMux()
-	mux.HandleFunc("/put", putHandler)
-	mux.HandleFunc("/get", getHandler)
-
-	// Wrap your handlers with the LoadAndSave() middleware.
-	http.ListenAndServe(":4000", sessionManager.LoadAndSave(mux))
-}
-
-func putHandler(w http.ResponseWriter, r *http.Request) {
-	// Store a new key and value in the session data.
-	sessionManager.Put(r.Context(), "message", "Hello from a session!")
-}
-
-func getHandler(w http.ResponseWriter, r *http.Request) {
-	// Use the GetString helper to retrieve the string value associated with a
-	// key. The zero value is returned if the key does not exist.
-	msg := sessionManager.GetString(r.Context(), "message")
-	io.WriteString(w, msg)
+	fmt.Println("Hello, world")
 }
