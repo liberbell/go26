b33db5177f4edf49ba67179243accd3fe780702d
Liberbell <liberbell@gmail.com>
2023-12-30 14:26:26 +0900

    main3

--- lesson6/main.go
+++ lesson6/main.go
@@ -7,5 +7,12 @@ func main() {
 	fmt.Println(users)
 
 	users["prefer@example.com"] = "prefer example"
+	users["lois@example.com"] = "lois example"
+	users["meg@example.com"] = "meg example"
+	users["chris@example.com"] = "chris example"
+	users["stewie@example.com"] = "stewie example"
+	users["brian@example.com"] = "brian example"
+
+	fmt.Println(users["meg@example.com"])
 	fmt.Println(users)
 }
