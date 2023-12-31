2de94c0c9753188bd8410faaab6ac96c2040bd73
Liberbell <liberbell@gmail.com>
2023-12-31 23:19:40 +0900

    main2

--- lesson8/main.go
+++ lesson8/main.go
@@ -1,8 +1,17 @@
 package main
 
-import "log"
+import (
+	"fmt"
+	"log"
+)
 
 func main() {
 	number := 23
 	log.Println("Let`s talk about", number)
+
+	if number < 0 {
+		fmt.Println(number, "is negatie")
+	} else {
+		fmt.Println(number, "is zero or positive")
+	}
 }
