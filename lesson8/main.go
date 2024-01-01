c3f77381395a11d228c8b124731fed8834425ac3
Liberbell <liberbell@gmail.com>
2024-01-01 22:20:13 +0900

    main3

--- lesson8/main.go
+++ lesson8/main.go
@@ -16,4 +16,15 @@ func main() {
 	} else {
 		fmt.Println(number, "has multiple digits")
 	}
+
+	i := 2
+	fmt.Print("Write ", i, " as ")
+	switch i {
+	case 1:
+		fmt.Println("One")
+	case 2:
+		fmt.Println("Two")
+	case 3:
+		fmt.Println("Three")
+	}
 }
