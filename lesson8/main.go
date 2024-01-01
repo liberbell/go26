35c6abbba3917a9bc0908c2727ab58d8e89a613b
Liberbell <liberbell@gmail.com>
2024-01-01 22:22:49 +0900

    main5

--- lesson8/main.go
+++ lesson8/main.go
@@ -29,8 +29,10 @@ func main() {
 		fmt.Println("Three")
 	}
 
-	switch time.Now() {
-	case time.Monday, time.Tuesday:
+	switch time.Now().Weekday() {
+	case time.Saturday, time.Sunday:
 		fmt.Println("It`s the weekend")
+	default:
+		fmt.Println("It`s a weekday")
 	}
 }
