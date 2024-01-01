dd54d0eb70f9ef0ea679faf548e56ee937e35a36
Liberbell <liberbell@gmail.com>
2024-01-01 22:17:43 +0900

    man2

--- lesson8/main.go
+++ lesson8/main.go
@@ -6,12 +6,14 @@ import (
 )
 
 func main() {
-	number := 9
+	number := 23
 	log.Println("Let`s talk about", number)
 
 	if number < 0 {
 		fmt.Println(number, "is negatie")
 	} else if number < 10 {
 		fmt.Println(number, "has 1 digit")
+	} else {
+		fmt.Println(number, "has multiple digits")
 	}
 }
