76d4f0d93708bc3d5c904b4e8c3429f5d8faf0d7
Liberbell <liberbell@gmail.com>
2024-01-02 12:21:37 +0900

    main4

--- lesson9/main.go
+++ lesson9/main.go
@@ -12,4 +12,6 @@ func main() {
 
 	aString := "One morning, when Gregor Samsa woke from troubled dreams, he found himself transformed in his bed"
 	fmt.Println(aString)
+
+	aMap := make([string]string{"dog": "Scoby Doo", "cat": "Garfield", "mouse": "Jerry", "platybus": "Perry"})
 }
