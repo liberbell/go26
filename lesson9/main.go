c9b6f8255260ad840a41d06c789162e24688255d
Liberbell <liberbell@gmail.com>
2024-01-02 12:31:34 +0900

    main9

--- lesson9/main.go
+++ lesson9/main.go
@@ -11,6 +11,10 @@ func main() {
 	aSlice := []string{"Fry", "Leela", "Bender", "Amy", "Hubert", "Zoidberg", "Hermes", "Scruffy", "Nibbler"}
 	fmt.Println(aSlice)
 
+	for i, val := range aSlice {
+		fmt.Printf("%d %v ", i, val)
+	}
+
 	aString := "One morning, when Gregor Samsa woke from troubled dreams, he found himself transformed in his bed"
 	fmt.Println(aString)
 
