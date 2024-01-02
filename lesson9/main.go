b08158ddb8a215ef3444f2c7872446f8fad069a1
Liberbell <liberbell@gmail.com>
2024-01-02 12:16:53 +0900

    main2

--- lesson9/main.go
+++ lesson9/main.go
@@ -1,10 +1,12 @@
 package main
 
+import "fmt"
+
 func main() {
 	// for i := 1
 
 	// fmt.Println(i)
 
 	aSlice := []string{"Fry", "Leela", "Bender", "Amy", "Hubert", "Zoidberg", "Hermes", "Scruffy", "Nibbler"}
-	fm.Println(aSlice)
+	fmt.Println(aSlice)
 }
