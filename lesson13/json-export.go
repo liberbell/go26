7c5c21d5973e6083baff374e05f409a1f3dd6449
Liberbell <liberbell@gmail.com>
2024-01-05 08:45:10 +0900

    json-export3

--- lesson13/json-export.go
+++ lesson13/json-export.go
@@ -1,5 +1,7 @@
 package main
 
+import "os"
+
 type Person struct {
 	Name   string `json:"name"`
 	Age    int    `json: "age"`
@@ -10,5 +12,10 @@ func main() {
 	people := []Person{
 		{"Peter", 44, "Male"},
 		{"Lois", 23, "Femail"},
+	}
+
+	file, err := os.Create("export.json")
+	if err != nil {
+		panic(err)
 	}
 }
