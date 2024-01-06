34249a02685ffc8c5d6929e017d4d8d167d07845
Liberbell <liberbell@gmail.com>
2024-01-06 11:52:13 +0900

    main6

--- lesson15/main.go
+++ lesson15/main.go
@@ -9,9 +9,14 @@ func Home(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "This is the home page.")
 }
 
+func About(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "This is the about page.")
+}
+
 func main() {
 
 	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
 
 	// http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 	// 	n, err := fmt.Fprintf(w, "Hello, world")
@@ -20,8 +25,6 @@ func main() {
 	// 	}
 	// 	fmt.Println(fmt.Sprintf("Bytes written: %d", n))
 	// })
-
-	http.HandleFunc("/about", func(w http.ResponseWriter, r *http.Request) {})
 
 	http.ListenAndServe("localhost:8000", nil)
 }
