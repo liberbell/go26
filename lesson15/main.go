97d42e2e392e93b11f952fb4100b94e417605bd1
Liberbell <liberbell@gmail.com>
2024-01-08 14:00:31 +0900

    main4

--- lesson15/main.go
+++ lesson15/main.go
@@ -9,11 +9,11 @@ import (
 const portNumber = ":8000"
 
 func Home(w http.ResponseWriter, r *http.Request) {
-
+	renderTemplate(w, "home_page.tpml")
 }
 
 func About(w http.ResponseWriter, r *http.Request) {
-
+	renderTemplate(w, "about_page.tpml")
 }
 
 func renderTemplate(w http.ResponseWriter, tpml string) {
