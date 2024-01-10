f4672a572026bfda6746b12386cf24f40df992b0
Liberbell <liberbell@gmail.com>
2024-01-11 08:37:06 +0900

    handlers2

--- lesson15/handlers.go
+++ lesson15/handlers.go
@@ -5,9 +5,9 @@ import (
 )
 
 func Home(w http.ResponseWriter, r *http.Request) {
-	RenderTemplate(w, "home_page.tpml")
+	RenderTemplateTemp(w, "home_page.tpml")
 }
 
 func About(w http.ResponseWriter, r *http.Request) {
-	RenderTemplate(w, "about_page.tpml")
+	RenderTemplateTemp(w, "about_page.tpml")
 }
