0257dd01a0bd38d06514839cd11fd8b65cb42f15
Liberbell <liberbell@gmail.com>
2024-01-11 08:44:14 +0900

    render9

--- lesson15/render.go
+++ lesson15/render.go
@@ -3,11 +3,12 @@ package main
 import (
 	"fmt"
 	"html/template"
-	"log"
 	"net/http"
 )
 
-func RenderTemplateTemp(w http.ResponseWriter, tpml string) {
+func RenderTemplate(w http.ResponseWriter, tpml string) {
+	tc, err := createTemplateCache()
+
 	parsedTemplate, _ := template.ParseFiles("./templates/"+tpml, "./templates/base.tpml")
 	err := parsedTemplate.Execute(w, nil)
 	if err != nil {
@@ -15,43 +16,43 @@ func RenderTemplateTemp(w http.ResponseWriter, tpml string) {
 	}
 }
 
-var tc = make(map[string]*template.Template)
+// var tc = make(map[string]*template.Template)
 
-func RenderTemplate(w http.ResponseWriter, t string) {
-	var tmpl *template.Template
-	var err error
+// func RenderTemplate(w http.ResponseWriter, t string) {
+// 	var tmpl *template.Template
+// 	var err error
 
-	_, inMap := tc[t]
-	if !inMap {
-		// some cache
-		log.Println("creating template and add cache")
-		err = createTemplateCache(t)
-		if err != nil {
-			log.Println(err)
-		}
+// 	_, inMap := tc[t]
+// 	if !inMap {
+// 		// some cache
+// 		log.Println("creating template and add cache")
+// 		err = createTemplateCache(t)
+// 		if err != nil {
+// 			log.Println(err)
+// 		}
 
-	} else {
-		log.Println("using cached template")
-	}
+// 	} else {
+// 		log.Println("using cached template")
+// 	}
 
-	tmpl = tc[t]
-	err = tmpl.Execute(w, nil)
-	if err != nil {
-		log.Println(err)
-	}
-}
+// 	tmpl = tc[t]
+// 	err = tmpl.Execute(w, nil)
+// 	if err != nil {
+// 		log.Println(err)
+// 	}
+// }
 
-func createTemplateCache(t string) error {
-	templates := []string{
-		fmt.Sprintf("./templates/%s", t),
-		"./templates/base.tpml",
-	}
+// func createTemplateCache(t string) error {
+// 	templates := []string{
+// 		fmt.Sprintf("./templates/%s", t),
+// 		"./templates/base.tpml",
+// 	}
 
-	tmpl, err := template.ParseFiles(templates...)
-	if err != nil {
-		return err
-	}
-	tc[t] = tmpl
+// 	tmpl, err := template.ParseFiles(templates...)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	tc[t] = tmpl
 
-	return nil
-}
+// 	return nil
+// }
