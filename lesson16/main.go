24addbce199402ed9a3e208ee981038cdb7a9574
Liberbell <liberbell@gmail.com>
2024-01-15 08:26:24 +0900

    main1

--- lesson16/main.go
+++ lesson16/main.go
@@ -1,16 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"net/http"
-)
-
-const portNumber = "8000"
-
 func main() {
-	http.HandleFunc("/", Home)
-	http.HandleFunc("/about", About)
 
-	fmt.Println(fmt.Sprintf("Starting server on port: %d", portNumber))
-	_ = http.ListenAndServe(portNumber, nil)
 }
