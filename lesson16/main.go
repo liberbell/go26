bee163724c589ab7fc3ec57683daef8ce50c0e8c
Liberbell <liberbell@gmail.com>
2024-01-15 08:28:27 +0900

    main2

--- lesson16/main.go
+++ lesson16/main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"fmt"
+)
 
+func main() {
+	var whatToSay string
+	var i int
+
+	whatToSay = "Goodbye cruel world"
+	fmt.Println(whatToSay)
+
+	i = 8
+	fmt.Println("i is set to: ", i)
 }
