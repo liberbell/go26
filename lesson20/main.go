acc4252b2b10ad14bca17d144eef8b36ad5210de
Liberbell <liberbell@gmail.com>
2024-01-16 09:47:06 +0900

    main3

--- lesson20/main.go
+++ lesson20/main.go
@@ -1,9 +1,14 @@
 package main
 
+import "log"
+
 func main() {
 	var myString string
 	var myInt int
 
 	myString = "Hi"
 	myInt = 21
+
+	mySecondString := "another string"
+	log.Println(myString, mySecondString, myInt)
 }
