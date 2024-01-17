e2f2cca0cbc67c2aeea230c5609d3904af69d53d
Liberbell <liberbell@gmail.com>
2024-01-17 09:38:02 +0900

    main4

--- lesson20/main.go
+++ lesson20/main.go
@@ -17,6 +17,7 @@ func main() {
 	myMap := make(map[string]string)
 	myMap["dog"] = "Samson"
 	myMap["other-dog"] = "Cassie"
+	myMap["dog"] = "fido"
 
 	log.Println(myMap["dog"])
 	log.Println(myMap["other-dog"])
