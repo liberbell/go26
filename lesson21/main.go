427df329008bca525172352aafca580900f7915b
Liberbell <liberbell@gmail.com>
2024-01-18 09:45:12 +0900

    main7

--- lesson21/main.go
+++ lesson21/main.go
@@ -26,10 +26,10 @@ func main() {
 	if myNum > 99 && !isTrue {
 		log.Println("myNum is greater than 100 and isTrue is set to true")
 	} else if myNum < 100 && isTrue {
-
+		log.Println("1")
 	} else if myNum == 101 || isTrue {
-
+		log.Println("2")
 	} else if myNum > 1000 && isTrue == false {
-
+		log.Println("3")
 	}
 }
