8f4bde58fb4635f61de4818248ac8cf9b1806a43
Liberbell <liberbell@gmail.com>
2024-01-19 08:45:11 +0900

    main2

--- lesson22/main.go
+++ lesson22/main.go
@@ -6,4 +6,10 @@ func main() {
 	for i := 0; i < 10; i++ {
 		log.Println(i)
 	}
+
+	animals := []string{"dog", "cat", "horse", "pig", "cow"}
+
+	for i, animal := range animals {
+		log.Println(i, animal)
+	}
 }
