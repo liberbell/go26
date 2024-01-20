fa02ff249021e03d27db6b63ca60ca0b0a92dfc8
Liberbell <liberbell@gmail.com>
2024-01-20 10:45:29 +0900

    main2

--- lesson23/main.go
+++ lesson23/main.go
@@ -4,3 +4,14 @@ type Animal interface {
 	Say() string
 	NumberOfLegs() int
 }
+
+type Dog struct {
+	Name  string
+	Breed string
+}
+
+type Gorilla struct {
+	Name          string
+	Colour        string
+	NumberOfTeeth int
+}
