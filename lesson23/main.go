bdfbbcf559dfe4ea9931ebd4cad4a4606aceff9a
Liberbell <liberbell@gmail.com>
2024-01-20 10:55:31 +0900

    main6

--- lesson23/main.go
+++ lesson23/main.go
@@ -24,17 +24,17 @@ func main() {
 		Breed: "Poppy",
 	}
 
-	PrintInfo(dog)
+	PrintInfo(&dog)
 }
 
 func PrintInfo(a Animal) {
 	fmt.Println("This animal says: ", a.Says(), "and has ", a.NumberOfLegs(), " legs.")
 }
 
-func (d Dog) Says() string {
+func (d *Dog) Says() string {
 	return "woof"
 }
 
-func (d Dog) NumberOfLegs() int {
+func (d *Dog) NumberOfLegs() int {
 	return 4
 }
