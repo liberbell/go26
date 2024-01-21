0906207c75c48d4df8c46fd5fe2d1d4ee1a128bf
Liberbell <liberbell@gmail.com>
2024-01-21 13:57:48 +0900

    main6

--- lesson24/main.go
+++ lesson24/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"command-line-arguments/Users/hideakiehara/Go/src/github.com/tsawler/myniceprogram/helpers/helpers.go"
 	"log"
 )
 
+const numPool = 10
+
 func CalculateValue(intChan chan int) {
-	
+	randomNumber := helpers.RandomNumber(numPool)
 }
 
 func main() {
