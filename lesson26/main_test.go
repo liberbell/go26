3fd0e9f5382efae013f8cf28be9cd72538e4d7e4
Liberbell <liberbell@gmail.com>
2024-01-23 08:42:33 +0900

    main6

--- lesson26/main_test.go
+++ lesson26/main_test.go
@@ -13,7 +13,7 @@ func TestDevide(t *testing.T) {
 func TestBadDevide(t *testing.T) {
 	_, err := divide(10.0, 0)
 	if err != nil {
-		t.Error("Got an error when we should not have")
+		t.Error("Dig not get an error when we should not have")
 	}
 
 }
