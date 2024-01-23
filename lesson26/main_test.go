64786b662b785f0b5b6b97bbd38c383efc83ed10
Liberbell <liberbell@gmail.com>
2024-01-24 08:38:31 +0900

    main_test2

--- lesson26/main_test.go
+++ lesson26/main_test.go
@@ -2,6 +2,16 @@ package main
 
 import "testing"
 
+var tests []struct {
+	name string
+	divident float32
+	divisor float32
+	expected float32
+	isErr bool
+}{
+	{"valid-data": 100.0, 10.0, 10.0, false}
+}
+
 func TestDevide(t *testing.T) {
 	_, err := divide(10.0, 1.0)
 	if err != nil {
