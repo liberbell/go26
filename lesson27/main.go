b1340f88235eaf4663f9a15f08cd39b96815e37b
Liberbell <liberbell@gmail.com>
2024-01-25 08:36:35 +0900

    main3

--- lesson27/main.go
+++ lesson27/main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 
-	http.HandlerFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("Hello world")
 	})
 }
