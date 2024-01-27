6700c020b6195953a081a80a754476c274907047
Liberbell <liberbell@gmail.com>
2024-01-27 23:27:10 +0900

    handlers3

--- lesson27/handlers.go
+++ lesson27/handlers.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"fmt"
-	"html/template"
 	"net/http"
 )
 
@@ -12,13 +10,4 @@ func Home(w http.ResponseWriter, r *http.Request) {
 
 func About(w http.ResponseWriter, r *http.Request) {
 	renderTemplate(w, "about_page.tmpl")
-}
-
-func renderTemplate(w http.ResponseWriter, tmpl string) {
-	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
-	err := parsedTemplate.Execute(w, nil)
-	if err != nil {
-		fmt.Println("error parsing template: ", err)
-		return
-	}
 }
