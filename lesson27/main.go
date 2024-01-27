35be001c943913e02d04646f096fc3ea228a331a
Liberbell <liberbell@gmail.com>
2024-01-27 22:43:21 +0900

    main2

--- lesson27/main.go
+++ lesson27/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
@@ -16,7 +17,12 @@ func About(w http.ResponseWriter, r *http.Request) {
 }
 
 func renderTemplate(w http.ResponseWriter, tmpl string) {
-
+	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
+	err := parsedTemplate.Execute(w, nil)
+	if err != nil {
+		fmt.Println("error parsing template: ", err)
+		return
+	}
 }
 
 func main() {
