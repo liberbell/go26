3546f52670fdb552a4b735066fa2f8d2c398d050
Liberbell <liberbell@gmail.com>
2024-01-31 08:34:02 +0900

    render2

--- lesson27/pkg/render/render.go
+++ lesson27/pkg/render/render.go
@@ -3,11 +3,10 @@ package render
 import (
 	"fmt"
 	"html/template"
-	"log"
 	"net/http"
 )
 
-func RenderTemplateTest(w http.ResponseWriter, tmpl string) {
+func RenderTemplate(w http.ResponseWriter, tmpl string) {
 	parsedTemplate, _ := template.ParseFiles("./templates/"+tmpl, "./templates/base_layout.tmpl")
 	err := parsedTemplate.Execute(w, nil)
 	if err != nil {
@@ -16,38 +15,38 @@ func RenderTemplateTest(w http.ResponseWriter, tmpl string) {
 	}
 }
 
-var tc = make(map[string]*template.Template)
+// var tc = make(map[string]*template.Template)
 
-func RenderTemplate(w http.ResponseWriter, t string) {
-	var tmpl *template.Template
-	var err error
+// func RenderTemplate(w http.ResponseWriter, t string) {
+// 	var tmpl *template.Template
+// 	var err error
 
-	_, inMap := tc[t]
-	if !inMap {
-		log.Println("creating template and adding cache")
-		err = createTemplateCache(t)
-		if err != nil {
-			log.Println(err)
-		}
-	} else {
-		log.Println("using cached template")
-	}
-	tmpl = tc[t]
-	err = tmpl.Execute(w, nil)
-	if err != nil {
-		log.Println(err)
-	}
-}
+// 	_, inMap := tc[t]
+// 	if !inMap {
+// 		log.Println("creating template and adding cache")
+// 		err = createTemplateCache(t)
+// 		if err != nil {
+// 			log.Println(err)
+// 		}
+// 	} else {
+// 		log.Println("using cached template")
+// 	}
+// 	tmpl = tc[t]
+// 	err = tmpl.Execute(w, nil)
+// 	if err != nil {
+// 		log.Println(err)
+// 	}
+// }
 
-func createTemplateCache(t string) error {
-	templates := []string{
-		fmt.Sprintf("./templates/%s", t), "./templates/base_layout.tmpl",
-	}
-	tmpl, err := template.ParseFiles(templates...)
-	if err != nil {
-		return err
-	}
-	tc[t] = tmpl
+// func createTemplateCache(t string) error {
+// 	templates := []string{
+// 		fmt.Sprintf("./templates/%s", t), "./templates/base_layout.tmpl",
+// 	}
+// 	tmpl, err := template.ParseFiles(templates...)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	tc[t] = tmpl
 
-	return nil
-}
+// 	return nil
+// }
