3cec993a5e25e4c84bd9b2922919e8655f285094
Liberbell <liberbell@gmail.com>
2024-02-04 13:24:16 +0900

    handlers2

--- lesson27/pkg/handlers/handlers.go
+++ lesson27/pkg/handlers/handlers.go
@@ -7,17 +7,6 @@ import (
 	"github.com/tsawler/go-course/pkg/render"
 )
 
-type TemplateData struct {
-	StringMap map[string]string
-	IntMap    map[string]int
-	FloatMap  map[string]float32
-	Data      map[string]interface{}
-	CSRFToken string
-	Flash     string
-	Warning   string
-	Error     string
-}
-
 var Repo *Repository
 
 type Repository struct {
