c5f7fa4c0019cfe742caadae6d01d7c1d99e5be1
Liberbell <liberbell@gmail.com>
2024-02-05 15:34:24 +0900

    routes4

--- lesson27/cmd/web/routes.go
+++ lesson27/cmd/web/routes.go
@@ -13,5 +13,7 @@ func routes(app *config.AppConfig) http.Handler {
 	mux := pat.New()
 
 	mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
+	mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+
 	return mux
 }
