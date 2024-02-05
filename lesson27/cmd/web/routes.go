b7590752e8684f2ec5c8b60aef5ff1c33fa42f1d
Liberbell <liberbell@gmail.com>
2024-02-05 15:32:49 +0900

    routes3

--- lesson27/cmd/web/routes.go
+++ lesson27/cmd/web/routes.go
@@ -3,11 +3,15 @@ package main
 import (
 	"net/http"
 
+	"github.com/tsawler/go-course/pkg/handlers"
+
 	"github.com/bmizerany/pat"
 	"github.com/tsawler/go-course/pkg/config"
 )
 
 func routes(app *config.AppConfig) http.Handler {
 	mux := pat.New()
+
+	mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
 	return mux
 }
