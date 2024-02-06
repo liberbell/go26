93cdb62fc8f56364508a4304173e857d1148ffa3
Liberbell <liberbell@gmail.com>
2024-02-07 08:19:14 +0900

    middleware1

--- lesson27/cmd/web/middleware.go
+++ lesson27/cmd/web/middleware.go
@@ -20,5 +20,7 @@ func NoSurf(next http.Handler) http.Handler {
 	csrfHandler.SetBaseCookie(http.Cookie{
 		HttpOnly: true,
 		Path:     "/",
+		Secure:   false,
+		SameSite: http.SameSiteLaxMode,
 	})
 }
