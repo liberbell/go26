0ff2a5d5dd341b3f201cf7eb2a9556bd333f2707
Liberbell <liberbell@gmail.com>
2024-02-08 08:25:40 +0900

    handlers2

--- lesson27/pkg/handlers/handlers.go
+++ lesson27/pkg/handlers/handlers.go
@@ -35,6 +35,9 @@ func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
 	stringMap := make(map[string]string)
 	stringMap["test"] = "Hello again"
 
+	remoteIP := m.App.Session.Get(r.Context(), "remote_ip")
+	stringMap["remote_ip"] = remoteIP
+
 	render.RenderTemplate(w, "about_page.tmpl", &models.TemplateData{
 		StringMap: stringMap,
 	})
