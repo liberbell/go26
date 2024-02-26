9b1ea7c07c4ca4ab49d1d66ff7771378d49156a3
Liberbell <liberbell@gmail.com>
2024-02-27 08:59:29 +0900

    handlers3

--- lesson28/pkg/handlers/handlers.go
+++ lesson28/pkg/handlers/handlers.go
@@ -28,7 +28,7 @@ func NewHandlers(r *Repository) {
 func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
 	remoteIP := r.RemoteAddr
 	m.App.Session.Put(r.Context(), "remote_ip", remoteIP)
-	render.RenderTemplate(w, "home_page.tmpl", &models.TemplateData{})
+	render.RenderTemplate(w, r, "home_page.tmpl", &models.TemplateData{})
 }
 
 func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
@@ -38,7 +38,7 @@ func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
 	remoteIP := m.App.Session.GetString(r.Context(), "remote_ip")
 	stringMap["remote_ip"] = remoteIP
 
-	render.RenderTemplate(w, "about_page.tmpl", &models.TemplateData{
+	render.RenderTemplate(w, r, "about_page.tmpl", &models.TemplateData{
 		StringMap: stringMap,
 	})
 }
