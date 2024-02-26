7615abb9ea2feb9b865cde602393e698318c1944
Liberbell <liberbell@gmail.com>
2024-02-27 08:56:25 +0900

    render1

--- lesson28/pkg/render/render.go
+++ lesson28/pkg/render/render.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/tsawler/bookings2/pkg/models"
 
+	"github.com/justinas/nosurf"
 	"github.com/tsawler/bookings2/pkg/config"
 )
 
@@ -20,12 +21,12 @@ func NewTemplates(a *config.AppConfig) {
 	app = a
 }
 
-func AddDefaultData(td *models.TemplateData, r http.Request) *models.TemplateData {
+func AddDefaultData(td *models.TemplateData, r *http.Request) *models.TemplateData {
 	td.CSRFToken = nosurf.Token(r)
 	return td
 }
 
-func RenderTemplate(w http.ResponseWriter, r http.Request, tmpl string, td *models.TemplateData) {
+func RenderTemplate(w http.ResponseWriter, r *http.Request, tmpl string, td *models.TemplateData) {
 
 	var tc map[string]*template.Template
 	if app.UseCache {
@@ -41,7 +42,7 @@ func RenderTemplate(w http.ResponseWriter, r http.Request, tmpl string, td *mode
 
 	buf := new(bytes.Buffer)
 
-	td = AddDefaultData(td)
+	td = AddDefaultData(td, r)
 
 	_ = t.Execute(buf, td)
 
