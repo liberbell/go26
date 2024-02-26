1ce2112db83895db07b8ff1be34bb25e0b225774
Liberbell <liberbell@gmail.com>
2024-02-27 08:49:38 +0900

    routes2

--- lesson28/cmd/web/routes.go
+++ lesson28/cmd/web/routes.go
@@ -14,7 +14,7 @@ import (
 func routes(app *config.AppConfig) http.Handler {
 	mux := chi.NewRouter()
 	mux.Use(middleware.Recoverer)
-	// mux.Use(NoSurf)
+	mux.Use(NoSurf)
 	mux.Use(SessionLoad)
 
 	mux.Get("/", handlers.Repo.Home)
